0326a2c77712842365870f329aa46f31e6623d03
laizamariegonzales <laizamarie.gonzales.a@bulsu.edu.ph>
2022-12-19 12:17:40 +0800

    delete-okay

--- src/app/components/displayproducts/displayproducts.component.ts
+++ src/app/components/displayproducts/displayproducts.component.ts
@@ -1,22 +1,30 @@
-import { Component, OnInit } from '@angular/core';
-import {InventoryserviceService} from '../../dashboard/service/inventoryservice.service'
-import {Products} from '../../dashboard/interface/products'
+import { Component, OnInit, Input } from '@angular/core';
+import { InventoryserviceService } from '../../dashboard/service/inventoryservice.service';
+import { Products } from '../../dashboard/interface/products';
 import { Observable } from 'rxjs';
-
-
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-displayproducts',
   templateUrl: './displayproducts.component.html',
-  styleUrls: ['./displayproducts.component.scss']
+  styleUrls: ['./displayproducts.component.scss'],
 })
 export class DisplayproductsComponent implements OnInit {
   userlist!: Observable<Products[]>;
-  constructor(private service:InventoryserviceService) { }
+  
+  constructor(
+    private service: InventoryserviceService,
+    private http: HttpClient
+  ) {}
 
   ngOnInit(): void {
     this.userlist = this.service.fetchData();
-    console.log(this.userlist)
   }
 
+  delete(productID: number) {
+    this.http
+      .delete('https://639843c6044fa481d696f181.mockapi.io/Products/' + productID)
+      .subscribe((response) => console.log('Success!'));
+      console.log(productID)
+  }
 }
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { DialogComponent } from '../components/dialog/dialog.component';
+import { Products } from './interface/products';
 
 @Component({
   selector: 'app-dashboard',
